2083a2924cd410a17d039d689c24434633b7425f
Tomusange Henry <tomusangehenry02@gmail.com>
2018-10-12 10:42:54 +0300

    Changes to some pages

--- templates/static/main.js
+++ templates/static/main.js
@@ -2,7 +2,7 @@ function adminLog(){
     var adminEmail = document.forms["admin-login"]["email"].value;
     var adminPassword = document.forms["admin-login"]["password"].value;
 
-    if(adminEmail == "admin@admin.com" & adminPassword == "password"){
+    if(adminEmail === "admin@admin.com" && adminPassword === "password"){
         return true;
     }
     else{
@@ -16,8 +16,9 @@ function attendantLog(){
     var userEmail = document.forms["attendant-login"]["email"].value;
     var userPassword = document.forms["attendant-login"]["password"].value;
 
-    if(userEmail == "user@user.com" & userPassword == "password"){
+    if(userEmail === "user@user.com" && userPassword === "password"){
         return true;
+        
     }
     else{
         alert("Sorry: Wrong Login Information");
